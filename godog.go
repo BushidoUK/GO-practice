38898636bb46986e94b081b1f81cac88d4279378
WilliamThomas-sec <59974887+WilliamThomas-sec@users.noreply.github.com>
2020-05-03 20:50:48 +0100

    Add files via upload

---
+++ godog.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+ t"time"
+)
+
+func main(){
+  fmt.Println("  __      _")
+  fmt.Println("o'')}____//")
+  fmt.Println(" `_/      )")
+  fmt.Println(" (_(_/-(_/ ")
+  fmt.Println(t.Now())
+}
---
+++ gravity.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Create the constant earthsGravity
+  // here and set it to 9.80665
+  
+  // Here's where we print out the gravity:
+  
+  const earthsGravity = 9.80665
+
+  fmt.Println(earthsGravity)
+}
