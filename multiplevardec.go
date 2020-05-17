2c13b357d335b7caa59d64aaf1d884ee364eb32e
WilliamThomas-sec <59974887+WilliamThomas-sec@users.noreply.github.com>
2020-05-17 12:53:13 +0100

    Create multiplevardec.go

---
+++ multiplevardec.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main () {
+  // Define magicNum and powerLevel below:
+  var magicNum, powerLevel int32
+  magicNum = 2048
+  powerLevel = 9001
+  
+  fmt.Println("magicNum is:", magicNum, "powerLevel is:", powerLevel)
+    
+  // Define amount and unit below:
+  amount, unit := 10, "doll hairs"
+  
+  fmt.Println(amount, unit, ", that's expensive...")
+}
